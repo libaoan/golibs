43f94a240fb7dc4fd002addcba12aa1022d8933a
libaoan <baoan_008@163.com>
2021-07-05 07:50:33 +0800

    example for ioutil

--- src/std01/ioutil/ioutil.go
+++ src/std01/ioutil/ioutil.go
@@ -3,26 +3,75 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
-func main(){
-	ListAll("/root")
+func main() {
+	ListAll(".")
+	path := "./src/std01/ioutil/ioutil.go"
+	content, err := ReadAll(path)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(string(content))
+	}
+
+	// Discard
+	content, err = os.ReadFile(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	n, err := ioutil.Discard.Write(content)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("The length of content of file", path, n)
+	}
+
+	//TempDir and TempFile
+	newpath, err := ioutil.TempDir("", "prefix")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Construct new path", newpath)
+	}
+
+	f, err := ioutil.TempFile("", "prefix")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	n, err = f.Write(content)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Write content in file %s with length %d", f.Name(), n)
+	}
 }
 
-
-func ListAll(path string){
+func ListAll(path string) {
 	fileInfos, err := ioutil.ReadDir(path)
-        if err != nil {
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	for _, info := range fileInfos{
-		if info.IsDir(){
-			fmt.Println(info.Name())
-                        ListAll(path+"/"+info.Name())
-		}else{
-			fmt.Println(info.Name())
+	for _, info := range fileInfos {
+		if info.IsDir() {
+			fmt.Println(path + "/" + info.Name())
+			ListAll(path + "/" + info.Name())
+		} else {
+			fmt.Println(path + "/" + info.Name())
 		}
 	}
-	
+}
+
+func ReadAll(path string) ([]byte, error) {
+
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return ioutil.ReadAll(f)
 }
